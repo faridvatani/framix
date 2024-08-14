397fe7c9d32c21c5bc392af00bdd8789c3d7f64c
Farid Vatani <faridvatani@gmail.com>
2024-08-14 20:07:50 +0330

    refactor(header): make Navbar sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 backdrop-blur-md z-50">
+    <>
       <Banner />
-      <Navbar />
-    </header>
+      <header className="sticky top-0 backdrop-blur-md z-50">
+        <Navbar />
+      </header>
+    </>
   );
 };
