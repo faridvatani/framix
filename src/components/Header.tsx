c337e6bb4b12b00607167922b68759d42824a1d3
Farid Vatani <faridvatani@gmail.com>
2024-08-14 16:54:18 +0330

    feat: Implement Hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-md z-50">
       <Banner />
       <Navbar />
     </header>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,65 @@
+import Link from "next/link";
+import ArrowIcon from "@/assets/icons/arrow-w.svg";
+import CursorImage from "@/assets/images/cursor.png";
+import MessageImage from "@/assets/images/message.png";
+import Image from "next/image";
+
 export const Hero = () => {
-  return null;
+  return (
+    <div className="relative bg-black text-white py-[72px] sm:py-24 bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] overflow-clip">
+      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+      <div className="container relative">
+        <div className="flex items-center justify-center">
+          <Link
+            href="#"
+            className="inline-flex gap-3 border border-white/30 py-1 px-2 rounded-lg"
+          >
+            <span className="bg-[linear-gradient(to_right,#F87AFF,#FB93D0_25%,#FFDD99_50%,#C3F0B2_75%,#2FD8FE)] text-transparent bg-clip-text [-webkit-background-clip:text]">
+              Version 2.0 is here
+            </span>
+            <span className="inline-flex items-center gap-1">
+              <span>Read More</span>
+              <ArrowIcon />
+            </span>
+          </Link>
+        </div>
+        <div className="flex justify-center mt-8">
+          <div className="relative inline-flex">
+            <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center inline-flex">
+              One Task
+              <br /> at a Time
+            </h1>
+            <Image
+              src={CursorImage}
+              alt="Cursor Image"
+              height="200"
+              width="200"
+              className="absolute right-[476px] top-[108px] hidden sm:inline"
+            />
+            <Image
+              src={MessageImage}
+              alt="Message Image"
+              height="200"
+              width="200"
+              className="absolute left-[498px] top-[56px] hidden sm:inline"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-xl text-center mt-8 max-w-md">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes.
+          </p>
+        </div>
+        <div className="flex justify-center mt-8">
+          <button
+            type="button"
+            className="bg-white text-black py-3 px-5 rounded-lg font-medium"
+          >
+            Get for free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
