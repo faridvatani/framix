59b31459a61fc4c0f12d48b0810e578ac9c56516
Farid Vatani <faridvatani@gmail.com>
2024-08-14 21:57:05 +0330

    feat: Implement FAQs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FAQs } from "@/components/FAQs";
 import { Features } from "@/components/Features";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
@@ -12,6 +13,7 @@ export default function Home() {
       <LogoTicker />
       <Features />
       <ProductShowcase />
+      <FAQs />
     </main>
   );
 }
--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,51 @@
+"use client";
+import { FAQItems } from "@/data/data";
+import PlusIcon from "@/assets/icons/plus.svg";
+import MinusIcon from "@/assets/icons/minus.svg";
+import { useState } from "react";
+import clsx from "clsx";
+
+const AccordionItem = ({
+  question,
+  answer,
+}: {
+  question: string;
+  answer: string;
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <div
+      className="py-7 border-b border-white/70"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex items-center">
+        <span className="flex-1 text-lg font-bold">{question}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+      </div>
+      <div className={clsx("mt-4", { hidden: !isOpen, "": isOpen === true })}>
+        {answer}
+      </div>
+    </div>
+  );
+};
+
 export const FAQs = () => {
-  return null;
+  return (
+    <section className="bg-black text-white bg-gradient-to-b from-[#5D2CA8] to-black py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter sm:max-w-[648px] mx-auto">
+          Frequently asked questions
+        </h2>
+        <div className="mt-12 max-w-[648px] mx-auto">
+          {FAQItems.map((items, index) => (
+            <AccordionItem
+              key={index}
+              question={items.question}
+              answer={items.answer}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
