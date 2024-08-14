80a350349754bbcfdc928ce1eca4a38649e03554
Farid Vatani <faridvatani@gmail.com>
2024-08-15 00:07:21 +0330

    feat: Implement features animation

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,5 +1,5 @@
 import { featureItems } from "@/data/data";
-import EcosystemIcon from "@/assets/icons/ecosystem.svg";
+import { Feature } from "./Feature";
 
 export const Features = () => {
   return (
@@ -16,17 +16,12 @@ export const Features = () => {
           </p>
         </div>
         <div className="mt-16 flex flex-col sm:flex-row gap-4">
-          {featureItems.map((items, index) => (
-            <div
+          {featureItems.map((item, index) => (
+            <Feature
               key={index}
-              className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
-            >
-              <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
-                <EcosystemIcon />
-              </div>
-              <h3 className="mt-6 font-bold">{items.title}</h3>
-              <p className="mt-2 text-white/70">{items.description}</p>
-            </div>
+              title={item.title}
+              description={item.description}
+            />
           ))}
         </div>
       </div>
