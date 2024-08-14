96d8417a8d220be7668e15a0b43abcf49238d80e
Farid Vatani <faridvatani@gmail.com>
2024-08-14 20:32:03 +0330

    feat: Implement Features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Features } from "@/components/Features";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { LogoTicker } from "@/components/LogoTicker";
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <LogoTicker />
+      <Features />
     </main>
   );
 }
