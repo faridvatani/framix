798356b892d265aa5a9cdae2636b8b8ba1ca63c1
Farid Vatani <faridvatani@gmail.com>
2024-08-15 13:27:22 +0330

    feat: add "scroll to top" button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
+import ScrollToTop from "@/components/ScrollToTop";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <FAQs />
       <CallToAction />
       <Footer />
+      <ScrollToTop />
     </main>
   );
 }
